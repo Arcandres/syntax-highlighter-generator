8454b7b14512c613ff45d3d1236e1b40e8754e68
Andres Ardila <Arcandres@hotmail.com>
2018-09-10 11:04:08 -0500

    Events and event handlers

---
+++ js/j.js
@@ -0,0 +1,27 @@
+((d) => {
+  function syntax() {
+    preview.textContent = code.value;
+    hljs.highlightBlock(preview)
+    parsed.textContent = preview.parentNode.outerHTML.replace('id="preview"','')
+    changeTheme()
+  }
+
+  function changeTheme() {
+    theme.href = `themes/${themeSelector.value}.css`;
+    css.textContent = theme.outerHTML.replace('id="theme"','');
+  }
+
+  function lang() {
+    if (langSelector.value) {
+      for( let i = preview.classList.length; i > 0; i-- ) {
+        preview.classList.remove( preview.classList[0] );
+      }
+      preview.classList.add(langSelector.value)
+    }
+    syntax()
+  }
+
+  addEventListener('DOMContentLoaded', () =>{syntax(), lang(), addEventListener('keyup', syntax)})
+  themeSelector.addEventListener('change', changeTheme)
+  convert.addEventListener('click', lang)
+})(document);
